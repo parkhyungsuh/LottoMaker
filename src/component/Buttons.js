c57d568a24d0cbf0b98b15321a36a7c083be8b22
PHS <sonbbal20@gmail.com>
2020-10-15 15:49:38 +0900

    Add Button Component

--- src/component/Buttons.js
+++ src/component/Buttons.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import '../css/Buttons.css'
 
 const Buttons = () => {
     return (
-        <div>
-            버튼
+        <div className="button-container">
+            <ResetButton />
+            <AutoButton />
+            <SaveButton />
         </div>
     )
 }
 
+const ResetButton = () => {
+    return <button type='button'>리셋</button>
+}
+const AutoButton = () => {
+    return <button type='button'>자동</button>
+}
+const SaveButton = () => {
+    return <button type='button'>저장</button>
+}
 export default Buttons;
