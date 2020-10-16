3336f6ae3ad6096eca0c793a1cf653a50a1c7839
PHS <sonbbal20@gmail.com>
2020-10-17 08:25:24 +0900

    Add function select number

--- src/App.js
+++ src/App.js
@@ -7,11 +7,19 @@ import './css/main.css';
 const App = () => {
   const [select, setSelect] = useState([]);
   const [left, setLeft] = useState([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45]);
-
+  const selecting = ( number ) => {
+    if(select.includes(number)){
+      setSelect(select.filter( num => num !== number).sort(function(a,b){return a-b}));
+      setLeft(left.concat(number).sort(function(a,b){return a-b}));
+    } else {
+      setSelect(select.concat(number).sort(function(a,b){return a-b}));
+      setLeft(left.filter( num => num !== number).sort(function(a,b){return a-b}));  
+    }
+  }
   return ( 
     <div className="card">
       <SelectedNumber numbers={select} />
-      <SelectPannel />
+      <SelectPannel selecting={selecting} counter={select.length}/>
       <Buttons leftNumber={left} />
     </div>
   )
--- src/component/SelectPannel.js
+++ src/component/SelectPannel.js
@@ -1,19 +1,28 @@
 import React, { useState } from 'react';
 import '../css/SelectPannel.css'
 
-const SelectPannel = () => {    
+const SelectPannel = (props) => {    
     return (
         <div className="number-container">        
         {[...Array(45)].map((n,index) => {
-            return <Number num={index+1} key={index} />
+            return <Number num={index+1} key={index} selecting={props.selecting} counter={props.counter}/>
         })}
         </div>)
 }
 
 const Number = (props) => {
     const [picked, setPicked] = useState(false);
+    const picking = (num) => {
+        if(props.counter < 6) {
+            setPicked(!picked);
+            console.log(typeof num);
+            props.selecting(num);
+        } else {
+            alert('번호선택끝');
+        }
+    }
 
-    return <div className={`pannel-number${picked? ' picked':``}`} onClick={() => setPicked(!picked)}>{props.num}</div>
+    return <div className={`pannel-number${picked? ' picked':``}`} onClick={() => picking(props.num)}>{props.num}</div>
 }
 
 export default SelectPannel;
