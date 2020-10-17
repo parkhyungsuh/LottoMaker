014c5bb5331add10a0d285ee6b7315d3722f222a
PHS <sonbbal20@gmail.com>
2020-10-17 10:03:13 +0900

    modify Selectpannel picking method

--- src/component/SelectPannel.js
+++ src/component/SelectPannel.js
@@ -1,28 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../css/SelectPannel.css'
 
 const SelectPannel = (props) => {    
     return (
         <div className="number-container">        
         {[...Array(45)].map((n,index) => {
-            return <Number num={index+1} key={index} selecting={props.selecting} counter={props.counter}/>
+            return (
+                <Number  
+                    num={index+1} 
+                    key={index} 
+                    selecting={props.selecting} 
+                    counter={props.counter}
+                    checking={props.numbers.indexOf(index+1) === -1 ? false : true}
+                />
+            )
         })}
         </div>)
 }
 
 const Number = (props) => {
-    const [picked, setPicked] = useState(false);
     const picking = (num) => {
         if(props.counter < 6) {
-            setPicked(!picked);
-            console.log(typeof num);
             props.selecting(num);
         } else {
             alert('번호선택끝');
         }
     }
 
-    return <div className={`pannel-number${picked? ' picked':``}`} onClick={() => picking(props.num)}>{props.num}</div>
+    return <div className={`pannel-number${props.checking ? ' picked':``}`} onClick={() => picking(props.num)}>{props.num}</div>
 }
 
 export default SelectPannel;
